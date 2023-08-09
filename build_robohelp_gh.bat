abad63f6322603cc31aa07b64d4b0054ae271e67
suchitra <ksuchitra532@gmail.com>
2023-08-09 15:59:54 +0100

    commented out css file line for now

--- build_robohelp_gh.bat
+++ build_robohelp_gh.bat
@@ -61,7 +61,7 @@ rem output help tags files
 %helpTagsTool% -s="%basedir%Manual\contents" -o="%DESTDIR%\RoboHelp"
 
 rem append css fix to layout.css
-type "%basedir%RoboHelpFiles\layout_fix_append.css" >> "%DESTDIR%\RoboHelp\template\Charcoal_Grey\layout.css"
+@REM type "%basedir%RoboHelpFiles\layout_fix_append.css" >> "%DESTDIR%\RoboHelp\template\Charcoal_Grey\layout.css"
 
 @REM rem ************************************************** ZIP up the output
 @REM pushd %DESTDIR%\RoboHelp
