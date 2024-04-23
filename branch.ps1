138f7786260dd7f03818357709b91aa9ec96373d
suchitra <ksuchitra532@gmail.com>
2024-04-23 21:39:17 +0100

    fixing branch_name variable

---
+++ branch.ps1
@@ -0,0 +1,17 @@
+# Define the path to the HEAD file
+$headFilePath = ".git\refs\remotes\origin\HEAD"
+
+# Initialize the branch name variable
+$branchName = ""
+
+# Check if the HEAD file exists
+if (Test-Path $headFilePath -PathType Leaf) {
+    # Read the content of the HEAD file
+    $branchName = Get-Content $headFilePath -Raw
+
+    # Extract the branch name from the content
+    $branchName = $branchName -replace '^ref: refs/remotes/origin/', ''
+}
+
+# Output the branch name as an output variable
+Write-Output "##vso[task.setvariable variable=branch]$branchName"
