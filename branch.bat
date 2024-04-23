0481f850bf891e818fb4b653864347b549e9a213
suchitra <ksuchitra532@gmail.com>
2024-04-23 21:29:06 +0100

    fixing branch_name variable

--- branch.bat
+++ branch.bat
@@ -1,21 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Read the content of .git/refs/remotes/origin/HEAD
-set "git_head_file=.git\refs\remotes\origin\HEAD"
-
-rem Check if the file exists
-if exist "%git_head_file%" (
-    rem Read the content of the file into a variable
-    set /p "branch_name=" < "%git_head_file%"
-
-    rem Extract the branch name after "refs/remotes/origin/"
-    set "prefix=refs/remotes/origin/"
-    set "branch_name=!branch_name:*%prefix%=!"
-
-    rem Print the extracted branch name
-    echo Branch Name: !branch_name!
-) else (
-    echo Error: .git/refs/remotes/origin/HEAD not found
-    exit /b 1
+rem Read the content of .git/refs/remotes/origin/HEAD using type
+for /f "delims=" %%A in ('type .git\refs\remotes\origin\HEAD 2^>nul') do (
+    set "branch_ref=%%A"
 )
+
+rem Extract the branch name after "refs/remotes/origin/"
+set "prefix=refs/remotes/origin/"
+set "branch_name=!branch_ref:*%prefix%=!"
+
+rem Print the extracted branch name
+echo Branch Name: !branch_name!
