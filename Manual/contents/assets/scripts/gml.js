42a09e9100bc130c6dac636310f278fd3ae3b6a4
YYBartT <bteunis@opera.com>
2024-05-01 16:27:57 +0200

    docs(feature): documented the new dbg_text_separator function YoYoGames/GameMaker-Bugs#4998
    * Added function page and code example
    * Added "note" snippet about single-column controls that can't be shown together on a line, added this to function page

--- Manual/contents/assets/scripts/gml.js
+++ Manual/contents/assets/scripts/gml.js
@@ -197,6 +197,7 @@ export default function(hljs) {
     "is_mouse_over_debug_overlay", 
     "is_keyboard_used_debug_overlay", 
     "vertex_submit_ext", 
+    "dbg_text_separator", 
     "abs",
     "achievement_available",
     "achievement_event",
