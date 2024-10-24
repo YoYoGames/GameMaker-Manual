4a523c9b23fbeec21a0b0958ca863d7576cf9248
gurpreetsinghmatharoo <gurpreetsingh793@gmail.com>
2024-10-24 09:53:02 +0530

    docs(general): Remove requirement of URL constructor, fix placement and add styling

--- Manual/contents/assets/scripts/main_script.js
+++ Manual/contents/assets/scripts/main_script.js
@@ -2583,36 +2583,61 @@ document.addEventListener('DOMContentLoaded', (event) => {
 // ----------------------------------------------------------------------------------------------
 // Language select dropdown (create and navigate)
 // ----------------------------------------------------------------------------------------------
+// Create default style (full context view) and get parent elm
+var listStyle = `float: right;
+  background-color: #333;
+  border: 0;
+  color: white;
+  padding: 8px;
+  border-radius: 4px;`
 
-var myParent = document.body;
+var myParent = window.parent.document.body.getElementsByClassName("header")[0];
+
+// No context view style and parent elm
+if (myParent == undefined) {
+	myParent = window.parent.document.getElementById("rh-topic-header");
+	listStyle += `
+  margin-right: 11px;
+  margin-bottom: 11px;
+  margin-left: 30px;
+	`;
+}
 
 //Create array of options to be added
 var array = [
   { name: "English", code: "en" },
-  { name: "French", code: "fr" },
-  { name: "Spanish", code: "es" },
-  { name: "German", code: "de" },
-  { name: "Russian", code: "ru" },
-  { name: "Italian", code: "it" },
-  { name: "Polish", code: "pl" },
-  { name: "Brazilian", code: "br" },
-  { name: "Korean", code: "ko" },
-  { name: "Chinese", code: "zh" },
-  { name: "Japanese", code: "ja" }
-  ];
+  { name: "Français", code: "fr" },
+  { name: "Español", code: "es" },
+  { name: "Deutsch", code: "de" },
+  { name: "Русский", code: "ru" },
+  { name: "Italiano", code: "it" },
+  { name: "Polski", code: "pl" },
+  { name: "Português Brasileiro", code: "br" },
+  { name: "한국인", code: "ko" },
+  { name: "中国人", code: "zh" },
+  { name: "日本語", code: "ja" }
+];
 
 //Create and append select list
 var selectList = document.createElement("select");
 selectList.id = "mySelect";
-myParent.insertBefore(selectList, myParent.firstChild);
+selectList.style = listStyle;
+myParent.insertBefore(selectList, myParent.lastChild.nextSibling);
+
+//Create and append the options
+for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.value = JSON.stringify(array[i]);
+    option.text = array[i].name;
+    selectList.appendChild(option);
+} // end for
 
 // are we on the main site???? if so then lets find the index of the current language
-var urlCurrent = new URL(window.location.href);
-if (urlCurrent.hostname.endsWith( ".gamemaker.io")) {
+if (window.location.hostname.endsWith( ".gamemaker.io")) {
   // lets get the language from the pathname
-  const folders = urlCurrent.pathname.split("/");
-  if (folders.length >= 2) {
-    var language = folders[1];
+  const folders = window.location.pathname.split("/");
+  if (folders.length >= 3) {
+    var language = folders[2];
     // find the language index from the 
     for( var i=0; i<array.length; ++i) {
       if (array[i].code == language) {
@@ -2623,21 +2648,13 @@ if (urlCurrent.hostname.endsWith( ".gamemaker.io")) {
   } // end if
 } // end if
 
-//Create and append the options
-for (var i = 0; i < array.length; i++) {
-    var option = document.createElement("option");
-    option.value = JSON.stringify(array[i]);
-    option.text = array[i].name;
-    selectList.appendChild(option);
-} // end for
-
 selectList.addEventListener( "change", function(e) { 
   //var tg = selectList.target.value;
   //console.log("Hello entry " + tg.name + " " + tg.code + ", " + JSON.stringify(selectList)); 
   var index = selectList.selectedIndex;
   var entry = array[index];
-  var url = new URL(window.location.href);
-  var urlParams = url.searchParams;
+  var url = window.location.href;
+  //var urlParams = url.searchParams;
 
   // some logging for debugging
   //console.log("Hello entry " + JSON.stringify(array[index]));   
@@ -2649,24 +2666,17 @@ selectList.addEventListener( "change", function(e) {
   //}
 
   // check to see if this is localhost (i.e. we are testing locally)
-  if (!url.hostname.endsWith( ".gamemaker.io")) {
-    url.hostname = "manual.gamemaker.io";
-    url.protocol = "https";
-    url.port = "";
-    url.hash = `#t=${url.pathname.substring(1)}`;
-    url.pathname = `/beta/${entry.code}/`;
+  if (!url.includes( ".gamemaker.io")) {
+    url = `https://manual.gamemaker.io/monthly/${entry.code}/#t=${window.location.pathname.substring(1)}`;
+    console.log( `new url - ${url}`);
+    window.parent.location.href = url;
   } // end if
   else {
-    const folders = urlCurrent.pathname.split("/");
-    if (folders.length >= 2) {
-      folders[1] = entry.code;
-    }
-    url.pathname = `/${folders.join('/')}/`;    
+    const folders = window.location.pathname.split("/");
+    if (folders.length >= 3) {
+      folders[2] = entry.code;
+    } // end if
+    window.parent.location.pathname = `${folders.join('/')}`;
   }
 
-  console.log( `new url - ${url}`);
-  window.location.href = url;
 });
-
-
-
