b310991fe9c3686658a99d02dbb5933df9657df4
ksuchitra532 <62082236+ksuchitra532@users.noreply.github.com>
2026-09-11 13:17:48 +0100

    cherry-pick: develop: 8a7f25fe6cde8a5c0822945a7c5278aa6169a073 Update S3_PATH for GMS2 Manual Responsive HTML5

--- build_robohelp_gh.bat
+++ build_robohelp_gh.bat
@@ -107,8 +107,8 @@ if /i %robohelpPreset%=="GMS2 Manual Responsive HTML5 BETA" (
     set "S3_PATH=%S3_BUCKET%/Beta"
     echo Branch is develop - Choose BETA
 ) else if /i %robohelpPreset%=="GMS2 Manual Responsive HTML5" (
-    set "S3_PATH=%S3_BUCKET%/Green"
-    echo Branch is Main - Choose Green
+    set "S3_PATH=%S3_BUCKET%/LTS2026"
+    echo Branch is Main - Choose LTS2026
 ) else (
     set "S3_PATH=%S3_BUCKET%/LTS2026"
     echo Branch is neither develop nor main - Choose LTS2026
